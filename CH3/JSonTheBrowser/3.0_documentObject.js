eb9d7cbc6f412a0aa10408dd1cb45d98797fb076
Yeojun-Y <ikm3256@naver.com>
2022-11-04 23:51:36 +0900

    3.2

---
+++ CH3/JSonTheBrowser/3.0_documentObject.js
@@ -0,0 +1,14 @@
+const title = document.getElementById("title");
+
+console.dir(title);
+
+title.innerText = "hoho";
+
+const titles = document.getElementsByClassName("title");
+
+console.log(titles);
+
+const title1 = document.querySelector(".title h2");
+console.log(title1);
+
+title1.innerText = "hehihe";
