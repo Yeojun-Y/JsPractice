549555df7ee27c66fd6af3d209a28d09b345a8c2
Yeojun-Y <ikm3256@naver.com>
2022-11-11 23:07:33 +0900

    4.5

--- Ch4/Login/4.0_Input_Values.js
+++ Ch4/Login/4.0_Input_Values.js
@@ -1,9 +1,16 @@
 const SigninForm = document.querySelector("#signin-form");
 const SigninInput = document.querySelector("#signin-form input");
-const SigninButton = document.querySelector("#signin-form button");
-
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASS = "hidden";
 function onSignInsubmit(event) {
-    event.preventDefault();
-    console.log(SigninInput.value);
+  event.preventDefault();
+  const username = SigninInput.value;
+  SigninForm.classList.add(HIDDEN_CLASS);
+  localStorage.setItem("username", username);
+//   greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASS);
+  
 }
-SigninForm.addEventListener("submit", onSignInsubmit)
+
+SigninForm.addEventListener("submit", onSignInsubmit);
