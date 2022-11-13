36becff969009f8629e650e14267eb2ce5033d85
Yeojun-Y <ikm3256@naver.com>
2022-11-13 21:32:18 +0900

    4.7

--- Ch4/Login/4.0_Input_Values.js
+++ Ch4/Login/4.0_Input_Values.js
@@ -2,15 +2,27 @@ const SigninForm = document.querySelector("#signin-form");
 const SigninInput = document.querySelector("#signin-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
+
 function onSignInsubmit(event) {
   event.preventDefault();
-  const username = SigninInput.value;
+  const username1 = SigninInput.value;
   SigninForm.classList.add(HIDDEN_CLASS);
-  localStorage.setItem("username", username);
-//   greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASS);
-  
+  console.log(SigninForm.classList);
+  localStorage.setItem(USERNAME_KEY, username1);
+  paintGreeting(username1);
 }
-
-SigninForm.addEventListener("submit", onSignInsubmit);
+function paintGreeting(event) {
+  greeting.innerText = `Hello! ${event}`;
+  greeting.classList.remove(HIDDEN_CLASS);
+}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  //show form
+  SigninForm.classList.remove(HIDDEN_CLASS);
+  SigninForm.addEventListener("submit", onSignInsubmit);
+} else {
+  SigninForm.classList.add(HIDDEN_CLASS);
+  paintGreeting(savedUsername);
+  localStorage.removeItem(USERNAME_KEY);
+}
