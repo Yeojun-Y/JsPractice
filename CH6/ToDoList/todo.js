e46711772ee87e27adb80e41478832b1832293d7
Yeojun-Y <ikm3256@naver.com>
2022-11-25 12:17:21 +0900

    7.6

--- CH6/ToDoList/todo.js
+++ CH6/ToDoList/todo.js
@@ -1,20 +1,50 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
+let toDoListArray = [];
 
+const TODO_KEY = "todo";
+function saveToDo() {
+  localStorage.setItem(TODO_KEY, JSON.stringify(toDoListArray));
+}
 function showToDO(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  li.id = newToDo.id;
+  span.innerText = newToDo.text;
   li.appendChild(span);
-  span.innerText = newToDo;
+  const btn = document.createElement("button");
+  btn.innerHTML = "??X??";
+  btn.addEventListener("click", delToDo);
+  li.appendChild(btn);
   toDoList.appendChild(li);
+}
+function delToDo(event) {
+  const delLi = event.target.parentElement;
+  console.log(delLi.id);
+  delLi.remove();
+  //   localStorage.removeItem();
 }
 
 function handleToDoSubmit(event) {
+  const date = new Date();
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  showToDO(newToDo);
+  const newToDoObject = {
+    text: newToDo,
+    id: date.getTime() + Math.random().toFixed(4),
+  };
+  toDoListArray.push(newToDoObject);
+  showToDO(newToDoObject);
+  saveToDo();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDo = localStorage.getItem(TODO_KEY);
+if (savedToDo) {
+  const parsedToDo = JSON.parse(savedToDo);
+  toDoListArray = parsedToDo;
+  parsedToDo.forEach(showToDO);
+}
--- Ch5/Clock/quotes.js
+++ Ch5/Clock/quotes.js
@@ -1,43 +1,50 @@
 const quotes = [
   {
-    quote: "a",
-    author: "aa",
+    quote: "I never dreamed about success, I worked for it",
+    author: "Estee Lauder",
   },
   {
-    quote: "b",
-    author: "bb",
+    quote: "Do not try to be original, just try to be good.",
+    author: "Paul Rand",
   },
   {
-    quote: "c",
-    author: "cc",
+    quote: "Do not be afraid to give up the good to go for the great",
+    author: "John D. Rockefeller",
   },
   {
-    quote: "d",
-    author: "dd",
+    quote:
+      "If you cannot fly then run. If you cannot run, then walk. And if you cannot walk, then crawl, but whatever you do, you have to keep moving forward.",
+    author: "Martin Luther King Jr.",
   },
   {
-    quote: "e",
-    author: "ee",
+    quote:
+      "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    author: "Thomas Edison",
   },
   {
-    quote: "f",
-    author: "ff",
+    quote:
+      "The fastest way to change yourself is to hang out with people who are already the way you want to be",
+    author: "REid Hoffman",
   },
   {
-    quote: "g",
-    author: "gg",
+    quote:
+      "Money is like gasoline during a road trip. You do not want to run out of gas on your trip, but you are not doing a tour of gas stations",
+    author: "Tim O Reilly",
   },
   {
-    quote: "h",
-    author: "hh",
+    quote:
+      "Some people dream of success, while other people get up every morning and make it happen",
+    author: "Wayne Huizenga",
   },
   {
-    quote: "i",
-    author: "ii",
+    quote:
+      "The only thing worse than starting something and falling.. is not starting something",
+    author: "SEth Godin",
   },
   {
-    quote: "j",
-    author: "jj",
+    quote:
+      "If you really want to do something, you will find a way. If you do not, you will find an excuse.",
+    author: "Jim Rohn",
   },
 ];
 const quote = document.querySelector("#quote span:first-child");
