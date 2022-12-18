42cec2b37ac3b6bedbdf5f20ac15f390c9d0824b
Yeojun-Y <ikm3256@naver.com>
2022-12-19 01:20:45 +0900

    8.2.1.1

---
+++ CH6/ToDoList/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = api_key.API_KEY;
+function onGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  https: console.log(position);
+  console.log("HaHa, You live ", latitude, longitude, API_KEY);
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  console.log("Can't Find");
+}
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
---
+++ key.js
@@ -0,0 +1,3 @@
+const api_key = {
+  API_KEY: "9e6b7420b2dc24a8421a783f6390e6ee",
+};
